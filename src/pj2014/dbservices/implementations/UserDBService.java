ed6f12513dd34453579a1878ebe48724580d0ca5
ehoppe92 <elisabethhoppe@web.de>
2014-05-20 12:55:12 +0200

    configured classes order

--- src/pj2014/dbservices/implementations/UserDBService.java
+++ src/pj2014/dbservices/implementations/UserDBService.java
@@ -6,7 +6,7 @@ import javax.ejb.Remote;
 import javax.ejb.Singleton;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
-
+ 
 
 @Singleton
 @Remote(UserDBServiceRemote.class)
